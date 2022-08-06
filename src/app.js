93ad8f40b370d9cae45f003dc1f7ea53250b0898
Wishmastering <charsdb@gmail.com>
2022-08-06 03:22:00 +0000

    Ejercicio de 'excusa' en src

--- src/app.js
+++ src/app.js
@@ -7,5 +7,55 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let who = ["The  dog", "My grandma", "His turtle", "My bird"]; // 0 - 3
+
+  let action = ["ate", "peed", "crushed", "broke"]; // 0 - 3
+
+  let what = ["my homework", "the keys", "the car"]; // 0 - 2
+
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ]; // 0 - 4
+
+  let x = Math.floor(Math.random() * who.length);
+  // Aca queremos crear un numero al azar para  "action"
+
+  let y = Math.floor(Math.random() * action.length);
+  // Aca queremos crear un numero al azar para ""action"
+
+  let w = Math.floor(Math.random() * what.length);
+  // Aca queremos crear un numero al azar para "what"
+
+  let z = Math.floor(Math.random() * who.length);
+  // Aca queremos crear un numero al azar para "when"
+
+  console.log(who[x] + " " + action[y] + " " + what[w] + " " + when[z] + ".");
+
+  document.getElementById("excuse").innerHTML =
+    who[x] + " " + action[y] + " " + what[w] + " " + when[z] + ".";
 };
+
+/*
+let who =['The  dog','My grandma','His turtle','My bird'];  // 0 - 3
+let action = ['ate','peed','crushed','broke'];              // 0 - 3
+let what = ['my homework', 'the keys', 'the car'];          // 0 - 2
+let when = ['before the class','right on time','when I finished','during my lunch','while I was praying']; // 0 - 4
+
+function excuse() {
+    let x = Math.floor(Math.random()*who.length); // Aca queremos crear un numero al azar para  "action"
+    let y = Math.floor(Math.random()*action.length); // Aca queremos crear un numero al azar para ""action"
+    let w = Math.floor(Math.random()*what.length); // Aca queremos crear un numero al azar para "what"
+    let z = Math.floor(Math.random()*who.when); // Aca queremos crear un numero al azar para "when"
+    
+    console.log(who[x] + " " + action[y] + " "  + what[w] + " "  + when[z] + "."  );
+    // aca se imprimira la posicion "x" de los arrays "who" y "action"
+    // Tambien se imprimira la posicion "y" del array "what" 
+    // Y pot ultimo imptimita la posicion "z" del array "when"
+    // Ademas dejamos espacio en blanco " ", para que se vea bien la oracion.
+}
+*/
